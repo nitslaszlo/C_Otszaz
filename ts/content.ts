bfc13c55b88f413c3b011e805fb7e4198e4bb9f7
kovacskmate <31725451+kovacskmate@users.noreply.github.com>
2017-09-28 11:29:31 +0200

    Update content.ts

--- ts/content.ts
+++ ts/content.ts
@@ -1,15 +1,27 @@
-﻿import * as http from "http";
+import * as http from "http";
 import * as url from "url"; // űrlapokhoz, input kiolvasás
 import * as fs from "fs"; // file-kezelés
-
-export class Content {
-
+import { Vásárlás } from "./Vásárlás";
+export class Otszaz {
     Content(req: http.ServerRequest, res: http.ServerResponse): void {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-        res.write("<p>Hello TypeScript!</p>");
-
-
+        // tslint:disable-next-line:max-line-length
+        res.write("<body><pre style='font-family: Courier; font-size:18px; background: LightGray'>");
+        const v: Vásárlás[] = [];
+        let ssz: number = 1;
+        const sorok: string[] = fs.readFileSync("penztar.txt").toString().split("\r\n");
+        sorok.forEach((i) => {
+            if (i === "F") ssz++;
+            else v.push(new Vásárlás(ssz, i, 1));
+        });
+        res.write("2. feladat\nA fizetések száma: " + (ssz - 1));
+        let hányat: number = 0;
+        v.forEach(element => {
+            if (element.Ssz === 1) hányat++;
+        });
+        // tslint:disable-next-line:max-line-length
+        res.write("\n");
+        res.write("3. feladat\nAz első vásárló " + hányat + " darab árucikket vásárolt.");
         res.end();
     }
 }
-

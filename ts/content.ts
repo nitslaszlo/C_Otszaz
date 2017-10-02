b222ae70286c51aac97a04e4fb398c9f9cc403c2
kovacskmate <31725451+kovacskmate@users.noreply.github.com>
2017-10-02 14:04:07 +0200

    Update content.ts

--- ts/content.ts
+++ ts/content.ts
@@ -6,7 +6,13 @@ export class Otszaz {
     Content(req: http.ServerRequest, res: http.ServerResponse): void {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
         // tslint:disable-next-line:max-line-length
-        res.write("<body><pre style='font-family: Courier; font-size:18px; background: LightGray'>");
+        res.write("<body><form> <pre style='font-family: Courier; font-size:18px; background: LightGray'>");
+
+        const query: any = url.parse(req.url, true).query; // user input
+        const sorszam: string = query.sorszam === undefined ? "" : query.sorszam;
+        const arucikknev: string = query.arucikknev === undefined ? "" : query.arucikknev;
+        const darabszam: string = query.darabszam === undefined ? "" : query.darabszam;
+
         const v: Vásárlás[] = [];
         let ssz: number = 1;
         const sorok: string[] = fs.readFileSync("penztar.txt").toString().split("\r\n");
@@ -21,7 +27,42 @@ export class Otszaz {
         });
         // tslint:disable-next-line:max-line-length
         res.write("\n");
-        res.write("3. feladat\nAz első vásárló " + hányat + " darab árucikket vásárolt.");
-        res.end();
-    }
+        res.write("3. feladat\nAz első vásárló " + hányat + " darab árucikket vásárolt.\n");
+        res.write("<p>4. feladat\n Sorszám = " +
+            "<input type='text' name='sorszam' style='font-family:Courier; " +
+            "font - size: inherit; background:LightGray; ' value=" +
+            `'${sorszam}'><br>`);
+        res.write(" Árucikk neve = " +
+            "<input type='text' name='arucikknev' style='font-family:Courier; " +
+            "font - size: inherit; background:LightGray; ' value=" +
+            `'${arucikknev}'><br>`);
+        res.write(" Darabszám = " +
+            "<input type='text' name='darabszam' style='font-family:Courier; " +
+            "font - size: inherit; background:LightGray; ' value=" +
+            `'${darabszam}'><br>`);
+        res.write("</p><input type='submit' value='Frissítés'>\n");
+        res.write("5. feladat\n");
+        let alkalmak: number = 0;
+        v.forEach(element => {
+            if (element.Árucikk === arucikknev) alkalmak++;
+        });
+        // tslint:disable-next-line:max-line-length
+        res.write(alkalmak + " alkalommal vásároltak " + arucikknev + " árucikket.");
+        let elsosorszam: number = 0;
+        let utolsosorszam: number = 0;
+        for (let i: number = 0; i < v.length; i++) {
+            if (v[i].Árucikk === arucikknev) {
+                elsosorszam = v[i].Ssz; break;
+            }
+        }
+        res.write("\nAz első vásárlás sorszáma: " + elsosorszam);
+        v.forEach(element => {
+            if (element.Árucikk === arucikknev) {
+                element.Ssz 
+
+            }
+        });
+        res.write("\nAz utolsó vásárlás sorszáma: " + utolsosorszam + "</pre></form>");
+    res.end();
+}
 }
